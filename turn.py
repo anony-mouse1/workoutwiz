bf4422ccd7ca6359b1847abccab866d47509eb02
anony-mouse1 <mindblowingmaths@gmail.com>
2023-12-30 15:44:24 -0800

    changed creds format

--- turn.py
+++ turn.py
@@ -3,7 +3,6 @@ import os
 
 import streamlit as st
 from twilio.rest import Client
-
 logger = logging.getLogger(__name__)
 
 
@@ -18,8 +17,10 @@ def get_ice_servers():
 
     # Ref: https://www.twilio.com/docs/stun-turn/api
     try:
-        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+        
+        account_sid = st.secrets.db_credentials.user
+        auth_token = st.secrets.db_credentials.password
+        print(account_sid, auth_token)
     except KeyError:
         logger.warning(
             "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
